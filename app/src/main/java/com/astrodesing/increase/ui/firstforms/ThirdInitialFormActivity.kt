b1f28304111ef41cc87519f1a5fa5d7c46b5dcb5
andrew <andrewrfofficial@gmail.com>
2024-04-24 19:24:55 +0200

    Implementing income algorithm

--- app/src/main/java/com/astrodesing/increase/ui/firstforms/ThirdInitialFormActivity.kt
+++ app/src/main/java/com/astrodesing/increase/ui/firstforms/ThirdInitialFormActivity.kt
@@ -1,13 +1,12 @@
 package com.astrodesing.increase.ui.firstforms
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.astrodesing.increase.databinding.ActivityThirdInitialFormBinding
 import com.astrodesing.increase.model.Ingresos
 import com.astrodesing.increase.ui.dashboard.DashboardActivity
@@ -15,7 +14,6 @@ import com.astrodesing.increase.viewmodels.ThirdInitialFormViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -50,6 +48,55 @@ class ThirdInitialFormActivity : AppCompatActivity() {
                     .isNotEmpty() && binding.actvGastosMode.text.isNotEmpty() &&
                 binding.actvIngresosMode.text.toString().isNotEmpty()
             ) {
+                var diasDeLaSemanaGasto = ""
+                var diasDeLaSemanaIngreso = ""
+
+//
+//                if (binding.checkBoxIngreso1.isChecked) {
+//                    diasDeLaSemanaIngreso += "LUN"
+//                }
+//                if (binding.checkBoxIngreso2.isChecked) {
+//                    diasDeLaSemanaIngreso += "MAR"
+//                }
+//                if (binding.checkBoxIngreso3.isChecked) {
+//                    diasDeLaSemanaIngreso += "MIE"
+//                }
+//                if (binding.checkBoxIngreso4.isChecked) {
+//                    diasDeLaSemanaIngreso += "JUE"
+//                }
+//                if (binding.checkBoxIngreso5.isChecked) {
+//                    diasDeLaSemanaIngreso += "VIE"
+//                }
+//                if (binding.checkBoxIngreso6.isChecked) {
+//                    diasDeLaSemanaIngreso += "SAB"
+//                }
+//                if (binding.checkBoxIngreso7.isChecked) {
+//                    diasDeLaSemanaIngreso += "DOM"
+//                }
+//
+//                if (binding.checkBoxGasto1.isChecked) {
+//                    diasDeLaSemanaGasto += "LUN"
+//                }
+//                if (binding.checkBoxGasto2.isChecked) {
+//                    diasDeLaSemanaGasto += "MAR"
+//                }
+//                if (binding.checkBoxGasto3.isChecked) {
+//                    diasDeLaSemanaGasto += "MIE"
+//                }
+//                if (binding.checkBoxGasto4.isChecked) {
+//                    diasDeLaSemanaGasto += "JUE"
+//                }
+//                if (binding.checkBoxGasto5.isChecked) {
+//                    diasDeLaSemanaGasto += "VIE"
+//                }
+//                if (binding.checkBoxGasto6.isChecked) {
+//                    diasDeLaSemanaGasto += "SAB"
+//                }
+//                if (binding.checkBoxGasto7.isChecked) {
+//                    diasDeLaSemanaGasto += "DOM"
+//                }
+
+
                 CoroutineScope(Dispatchers.IO).launch {
                     thirdInitialFormViewModel.addIngreso(
                         Ingresos(
@@ -62,8 +109,9 @@ class ThirdInitialFormActivity : AppCompatActivity() {
                             diasDeLaSemana = ""
                         )
                     )
-                    runOnUiThread(){
-                        val intent = Intent(this@ThirdInitialFormActivity, DashboardActivity::class.java)
+                    runOnUiThread() {
+                        val intent =
+                            Intent(this@ThirdInitialFormActivity, DashboardActivity::class.java)
                         startActivity(intent)
                     }
 
@@ -82,19 +130,41 @@ class ThirdInitialFormActivity : AppCompatActivity() {
     private fun initLists() {
         CoroutineScope(Dispatchers.IO).launch {
             val modalidades = thirdInitialFormViewModel.getModalidades()
-            runOnUiThread(){
-                var spinnerItems:MutableList<String> = mutableListOf()
-                for (modalidad in modalidades){
+            runOnUiThread() {
+                val spinnerItems: MutableList<String> = mutableListOf()
+                for (modalidad in modalidades) {
                     spinnerItems.add(modalidad.modalidad)
                 }
                 val adapterSpinner =
-                    ArrayAdapter<String>(this@ThirdInitialFormActivity, android.R.layout.simple_spinner_dropdown_item)
+                    ArrayAdapter<String>(
+                        this@ThirdInitialFormActivity,
+                        android.R.layout.simple_spinner_dropdown_item
+                    )
                 adapterSpinner.addAll(spinnerItems)
+
                 binding.actvIngresosMode.setAdapter(adapterSpinner)
                 binding.actvGastosMode.setAdapter(adapterSpinner)
+
+                binding.actvIngresosMode.setOnItemClickListener { _, _, _, _ ->
+                    if (binding.actvIngresosMode.text.toString() == "Diario") {
+                        binding.checkBoxLayOutIngreso.visibility = View.VISIBLE
+                    } else {
+                        binding.checkBoxLayOutIngreso.visibility = View.INVISIBLE
+                    }
+                }
+
+                binding.actvGastosMode.setOnItemClickListener { _, _, _, _ ->
+                    if (binding.actvGastosMode.text.toString() == "Diario") {
+                        binding.checkBoxLayOutGasto.visibility = View.VISIBLE
+                    } else {
+                        binding.checkBoxLayOutGasto.visibility = View.INVISIBLE
+                    }
+                }
             }
         }
 
-        binding.actvGastosMode.select
+
     }
+
+
 }
